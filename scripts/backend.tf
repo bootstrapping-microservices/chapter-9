e8a0e06b56b552d27d6875bacc9984764102974b
Ashley Davis <ashley@codecapers.com.au>
2020-06-08 10:44:05 +1000

    Commented out backend setup for manual deployment.

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -1,8 +1,9 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "terraform"
-        storage_account_name = "terraform"
-        container_name       = "terraform"
-        key                  = "terraform.tfstate"
-    }
+    # Uncomment this to get it running in the CD pipeline.
+    # backend "azurerm" {
+    #     resource_group_name  = "terraform"
+    #     storage_account_name = "terraform"
+    #     container_name       = "terraform"
+    #     key                  = "terraform.tfstate"
+    # }
 }
