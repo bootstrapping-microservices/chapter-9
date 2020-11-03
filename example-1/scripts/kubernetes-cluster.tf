426eebb2c1af894ee1b2ae185fa1864e3b6c7a6b
Ashley Davis <ashley@codecapers.com.au>
2020-11-03 10:39:51 +1000

    Updated Kubernetes version no.

--- example-1/scripts/kubernetes-cluster.tf
+++ example-1/scripts/kubernetes-cluster.tf
@@ -4,7 +4,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     location            = var.location
     resource_group_name = azurerm_resource_group.flixtube.name
     dns_prefix          = var.app_name
-    kubernetes_version  = "1.17.7"
+    kubernetes_version  = "1.18.8"
 
     linux_profile {
         admin_username = var.admin_username
